cb9ec4848e7d7b4d22ca6436f611a1ac9a64471b
Renée Bäcker <renee.baecker@otrs.com>
2010-05-10 15:14:46 +0000

    Add sample code for dynamic stats.

---
+++ Kernel/System/Stats/Dynamic/DynamicStatsTemplate.pm
@@ -0,0 +1,206 @@
+# --
+# Kernel/System/Stats/Dynamic/DynamicStatsTemplate.pm - all advice functions
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# --
+# $Id: DynamicStatsTemplate.pm,v 1.1 2010-05-10 15:14:46 reb Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::Stats::Dynamic::DynamicStatsTemplate;
+
+use strict;
+use warnings;
+
+use Kernel::System::Queue;
+use Kernel::System::State;
+use Kernel::System::Ticket;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Object (
+        qw(DBObject ConfigObject LogObject UserObject TimeObject MainObject EncodeObject)
+        )
+    {
+        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
+    }
+
+    # created needed objects
+    $Self->{QueueObject}  = Kernel::System::Queue->new( %{$Self} );
+    $Self->{TicketObject} = Kernel::System::Ticket->new( %{$Self} );
+    $Self->{StateObject}  = Kernel::System::State->new( %{$Self} );
+
+    return $Self;
+}
+
+sub GetObjectName {
+    my ( $Self, %Param ) = @_;
+
+    return 'Sample Statistics';
+}
+
+sub GetObjectAttributes {
+    my ( $Self, %Param ) = @_;
+
+    # get state list
+    my %StateList = $Self->{StateObject}->StateList(
+        UserID => 1,
+    );
+
+    # get queue list
+    my %QueueList = $Self->{QueueObject}->GetAllQueues();
+
+    # get current time to fix bug#3830
+    my $TimeStamp = $Self->{TimeObject}->CurrentTimestamp();
+    my ($Date) = split /\s+/, $TimeStamp;
+    my $Today = sprintf "%s 23:59:59", $Date;
+
+    my @ObjectAttributes = (
+        {
+            Name             => 'State',
+            UseAsXvalue      => 1,
+            UseAsValueSeries => 1,
+            UseAsRestriction => 1,
+            Element          => 'StateIDs',
+            Block            => 'MultiSelectField',
+            Values           => \%StateList,
+        },
+        {
+            Name             => 'Created in Queue',
+            UseAsXvalue      => 1,
+            UseAsValueSeries => 1,
+            UseAsRestriction => 1,
+            Element          => 'CreatedQueueIDs',
+            Block            => 'MultiSelectField',
+            Translation      => 0,
+            Values           => \%QueueList,
+        },
+        {
+            Name             => 'Create Time',
+            UseAsXvalue      => 1,
+            UseAsValueSeries => 1,
+            UseAsRestriction => 1,
+            Element          => 'CreateTime',
+            TimePeriodFormat => 'DateInputFormat',    # 'DateInputFormatLong',
+            Block            => 'Time',
+            TimeStop         => $Today,
+            Values           => {
+                TimeStart => 'TicketCreateTimeNewerDate',
+                TimeStop  => 'TicketCreateTimeOlderDate',
+            },
+        },
+    );
+
+    return @ObjectAttributes;
+}
+
+sub GetStatElement {
+    my ( $Self, %Param ) = @_;
+
+    # search tickets
+    return $Self->{TicketObject}->TicketSearch(
+        UserID     => 1,
+        Result     => 'COUNT',
+        Permission => 'ro',
+        Limit      => 100_000_000,
+        %Param,
+    );
+}
+
+sub ExportWrapper {
+    my ( $Self, %Param ) = @_;
+
+    # wrap ids to used spelling
+    for my $Use (qw(UseAsValueSeries UseAsRestriction UseAsXvalue)) {
+        ELEMENT:
+        for my $Element ( @{ $Param{$Use} } ) {
+            next ELEMENT if !$Element || !$Element->{SelectedValues};
+            my $ElementName = $Element->{Element};
+            my $Values      = $Element->{SelectedValues};
+
+            if ( $ElementName eq 'QueueIDs' || $ElementName eq 'CreatedQueueIDs' ) {
+                ID:
+                for my $ID ( @{$Values} ) {
+                    next ID if !$ID;
+                    $ID->{Content} = $Self->{QueueObject}->QueueLookup( QueueID => $ID->{Content} );
+                }
+            }
+            elsif ( $ElementName eq 'StateIDs' || $ElementName eq 'CreatedStateIDs' ) {
+                my %StateList = $Self->{StateObject}->StateList( UserID => 1 );
+                ID:
+                for my $ID ( @{$Values} ) {
+                    next ID if !$ID;
+                    $ID->{Content} = $StateList{ $ID->{Content} };
+                }
+            }
+        }
+    }
+    return \%Param;
+}
+
+sub ImportWrapper {
+    my ( $Self, %Param ) = @_;
+
+    # wrap used spelling to ids
+    for my $Use (qw(UseAsValueSeries UseAsRestriction UseAsXvalue)) {
+        ELEMENT:
+        for my $Element ( @{ $Param{$Use} } ) {
+            next ELEMENT if !$Element || !$Element->{SelectedValues};
+            my $ElementName = $Element->{Element};
+            my $Values      = $Element->{SelectedValues};
+
+            if ( $ElementName eq 'QueueIDs' || $ElementName eq 'CreatedQueueIDs' ) {
+                ID:
+                for my $ID ( @{$Values} ) {
+                    next ID if !$ID;
+                    if ( $Self->{QueueObject}->QueueLookup( Queue => $ID->{Content} ) ) {
+                        $ID->{Content}
+                            = $Self->{QueueObject}->QueueLookup( Queue => $ID->{Content} );
+                    }
+                    else {
+                        $Self->{LogObject}->Log(
+                            Priority => 'error',
+                            Message  => "Import: Can' find the queue $ID->{Content}!"
+                        );
+                        $ID = undef;
+                    }
+                }
+            }
+            elsif ( $ElementName eq 'StateIDs' || $ElementName eq 'CreatedStateIDs' ) {
+                ID:
+                for my $ID ( @{$Values} ) {
+                    next ID if !$ID;
+
+                    my %State = $Self->{StateObject}->StateGet(
+                        Name  => $ID->{Content},
+                        Cache => 1,
+                    );
+                    if ( $State{ID} ) {
+                        $ID->{Content} = $State{ID};
+                    }
+                    else {
+                        $Self->{LogObject}->Log(
+                            Priority => 'error',
+                            Message  => "Import: Can' find state $ID->{Content}!"
+                        );
+                        $ID = undef;
+                    }
+                }
+            }
+        }
+    }
+    return \%Param;
+}
+
+1;

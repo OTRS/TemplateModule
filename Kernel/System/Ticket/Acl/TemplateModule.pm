485758f313a638dc1681c69e3b9486043812cc35
Manuel Hecht <manuel.hecht@otrs.com>
2012-11-20 21:52:28 +0000

    Improved license header.

--- Kernel/System/Ticket/Acl/TemplateModule.pm
+++ Kernel/System/Ticket/Acl/TemplateModule.pm
@@ -2,10 +2,10 @@
 # Kernel/System/Ticket/Acl/TemplateModule.pm - acl module
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: TemplateModule.pm,v 1.2 2012-11-20 19:13:11 mh Exp $
+# $Id: TemplateModule.pm,v 1.3 2012-11-20 21:52:26 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
-# the enclosed file COPYING for license information (GPL). If you
+# the enclosed file COPYING for license information (AGPL). If you
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.2 $) [1];
+$VERSION = qw($Revision: 1.3 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
